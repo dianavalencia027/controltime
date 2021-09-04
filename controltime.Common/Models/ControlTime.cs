6a070defec45ba9895b2c62a6681087a236bc3f4
DIANA PATRICIA VALENCIA LOPEZ <dianavalencia027@gmail.com>
2021-09-04 16:28:54 -0500

    Add model, response and entity

---
+++ controltime.Common/Models/ControlTime.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace controltime.Common.Models
+{
+    public class ControlTime
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
---
+++ controltime.Functions/Entities/ControlTimeEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace controltime.Functions.Entities
+{
+    public class ControlTimeEntity : TableEntity
+    {
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public string Type { get; set; }
+
+        public bool Consolidated { get; set; }
+    }
+}
